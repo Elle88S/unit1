257dd89f0a3aa3a824c1278ab46970ca7a442094
Elle <elle.sokolova92@gmail.com>
2024-01-26 15:12:11 +0300

    modify for testng

---
+++ src/test/java/ru/netology/TestCashBackHackService.java
@@ -0,0 +1,52 @@
+package ru.netology;
+
+
+import org.testng.annotations.Test;
+import ru.netology.service.CashBackHackService;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class TestCashBackHackService {
+    @Test
+    public void upperLimit(){
+        CashBackHackService cashBack = new CashBackHackService();
+
+        int actual = cashBack.remain(999);
+        int expected = 1;
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void beyondUpperLimit (){
+        CashBackHackService cashBack = new CashBackHackService();
+
+        int actual = cashBack.remain(1001);
+        int expected = 999;
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void bottomLine (){
+        CashBackHackService cashBack = new CashBackHackService();
+
+        int actual = cashBack.remain(1);
+        int expected = 999;
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void ifNeedZeroAmount (){
+        CashBackHackService cashBack = new CashBackHackService();
+
+        int actual = cashBack.remain(1000);
+        int expected = 0;
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void ifNeedFullAmount (){
+        CashBackHackService cashBack = new CashBackHackService();
+
+        int actual = cashBack.remain(0);
+        int expected = 1000;
+        assertEquals (actual, expected);
+    }
+
+}
